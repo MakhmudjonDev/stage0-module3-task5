92038cad4185b8cc4d23df9fb53ad7c8e92e966a
MakhmudjonDev <104415570+MakhmudjonDev@users.noreply.github.com>
2023-05-01 16:20:54 +0500

    sixth commit

---
+++ src/main/java/lang/print/gaps/task5/Main.java
@@ -0,0 +1,9 @@
+package lang.print.gaps.task5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        NumberDivider n1 = new NumberDivider();
+        n1.divide(19);
+    }
+}
